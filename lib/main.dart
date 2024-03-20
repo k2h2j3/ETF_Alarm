98810ebfcbcd9e8bdfa51e8214fdbb3c83c15b77
kim <k2h2j3@gmail.com>
2024-03-20 12:14:14 +0900

    기본 알람앱 example

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+import 'package:eft_alarm/screens/home.dart';
+import 'package:flutter/material.dart';
+import 'package:alarm/alarm.dart';
+import 'package:flutter/services.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  await Alarm.init(showDebugLogs: true);
+
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: false),
+      home: const ExampleAlarmHomeScreen(),
+    ),
+  );
+}
